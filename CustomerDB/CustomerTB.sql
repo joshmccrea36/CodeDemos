95db78d03180096523b1702788cd28c9c580c870
Joshua McCrea <josh.mccrea@gmail.com>
2017-09-01 21:04:01 -0400

    c-sharpcorner ASP.NET WebAPI Paging Exampel

---
+++ CustomerDB/CustomerTB.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CustomerTB]
+(
+	[CustomerID] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Address] NCHAR(10) NULL, 
+    [Country] NCHAR(10) NULL, 
+    [City] NCHAR(10) NULL, 
+    [PhoneNo] NCHAR(10) NULL
+)
